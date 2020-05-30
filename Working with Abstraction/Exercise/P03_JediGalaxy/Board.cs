1e0dec13975858616c7d99a1a5ee76b4dfcccc8e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-30 20:31:57 +0300

    Add P03_JediGalaxy

---
+++ Working with Abstraction/Exercise/P03_JediGalaxy/Board.cs
@@ -0,0 +1,46 @@
+﻿namespace P03_JediGalaxy
+{
+    public class Board
+    {
+        private int[,] matrix;
+
+        public Board(int rows, int cols)
+        {
+            this.Matrix = new int[rows, cols];
+            this.InitializeMatrix();
+        }
+
+        public int[,] Matrix
+        {
+            get 
+            {
+                return this.matrix;
+            }
+
+            set
+            {
+                this.matrix = value;
+            }
+        }
+
+        private void InitializeMatrix()
+        {
+            int value = 0;
+
+            for (int row = 0; row < this.matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = value++;
+                }
+            }
+        }
+
+        public bool IsInside(int row, int col)
+        {
+            bool result = row >= 0 && row < this.matrix.GetLength(0) && col >= 0 && col < this.matrix.GetLength(1);
+
+            return result;
+        }
+    }
+}
