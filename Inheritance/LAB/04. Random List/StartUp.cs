bbe217f2e4527415ef472a29a2f63a8252dc7d02
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-20 22:17:15 +0300

    Add Random list

---
+++ Inheritance/LAB/04. Random List/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var list = new RandomList();
+            list.Add("dog");
+            list.Add("cat");
+            list.Add("puppy");
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
