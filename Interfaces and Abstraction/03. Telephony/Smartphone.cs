100625b44ba111e9eb91b2a1f8782f38171509c6
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-23 23:28:09 +0300

    Add Telephony

---
+++ Interfaces and Abstraction/03. Telephony/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿namespace P04.Telephony
+{
+    using System.Linq;
+
+    public class Smartphone : ICaller, IBrowser
+    {
+
+        public string Call(string phoneNumber)
+        {
+            if (phoneNumber.All(x => char.IsLetter(x)))
+            {
+                return "Invalid number!";
+            }
+
+            return $"Calling... {phoneNumber}";
+        }
+
+        public string Browse(string url)
+        {
+            if (url.Any(x => char.IsDigit(x)))
+            {
+                return "Invalid URL!";
+            }
+
+            return $"Browsing: {url}!";
+        }
+    }
+}
