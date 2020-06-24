e384e18565a2dda811699ca3232b63ce9afdad0c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-24 22:04:01 +0300

    Add Explicit interfaces

---
+++ Interfaces and Abstraction/09. Explicit Interfaces/IResident.cs
@@ -0,0 +1,11 @@
+﻿namespace P10.ExplicitInterfaces
+{
+    public interface IResident
+    {
+        string Name { get; }
+
+        string Country { get; }
+
+        string GetName();
+    }
+}
---
+++ Interfaces and Abstraction/09. Explicit Interfaces/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace P10.ExplicitInterfaces
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string line = Console.ReadLine();
+
+            while (line != "End")
+            {
+                string[] citizenArgs = line.Split();
+
+                string name = citizenArgs[0];
+                string country = citizenArgs[1];
+                int age = int.Parse(citizenArgs[2]);
+
+                Citizen citizen = new Citizen(name, age, country);
+
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
