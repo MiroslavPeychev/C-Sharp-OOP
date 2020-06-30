19f194594e6bdfb8d4955ccb1b0e17b990e61c56
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-30 23:40:07 +0300

    Add Class Box data

---
+++ Encapsulation/Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,71 @@
+﻿namespace ClassBoxData
+{
+    using System;
+
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Length));
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                ValidateParameter(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+
+        private void ValidateParameter(double value, string parameterName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{parameterName} cannot be zero or negative.");
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return 2 * (this.length * this.width) + this.CalculateLateralSurfaceArea();
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return 2 * (this.length * this.height + this.width * this.height);
+        }
+
+        public double CalculateVolume()
+        {
+            return this.width * this.height * this.length;
+        }
+    }
+}
---
+++ Encapsulation/Exercise/01. Class Box Data/ClassBoxData.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassBoxData
+{
+    using System;
+
+    public class ClassBoxData
+    {
+        public static void Main()
+        {
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():F2}");
+            }
+            catch (ArgumentException exeption)
+            {
+                Console.WriteLine(exeption.Message);
+            }
+        }
+    }
+}
