5e5d5e7776592928bb2e5150bb1d37e90e544a1c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-02 22:11:18 +0300

    Add Salary

---
+++ Encapsulation/LAB/02. Salary/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace PersonsInfo
+{
+   
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(
+            string firstName,
+            string lastName,
+            int age,
+            decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+       
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.Salary += this.Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
---
+++ Encapsulation/LAB/02. Salary/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var commandArgs = Console.ReadLine().Split();
+                var person = new Person(commandArgs[0],
+                                        commandArgs[1],
+                                        int.Parse(commandArgs[2]),
+                                        decimal.Parse(commandArgs[3]));
+
+                persons.Add(person);
+            }
+
+            var parcentage = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
