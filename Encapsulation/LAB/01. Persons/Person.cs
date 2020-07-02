f32f690a53ae339360d49c4f4762d89b357d4f2b
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-02 22:09:48 +0300

    Add Persons

---
+++ Encapsulation/LAB/01. Persons/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonsInfo
+{
+    using System;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        
+
+        public Person(
+            string firstName,
+            string lastName,
+            int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
---
+++ Encapsulation/LAB/01. Persons/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var commandArgs = Console.ReadLine().Split();
+                var person = new Person(commandArgs[0], commandArgs[1], int.Parse(commandArgs[2]));
+
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                   .ThenBy(p => p.Age)
+                   .ToList()
+                   .ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
