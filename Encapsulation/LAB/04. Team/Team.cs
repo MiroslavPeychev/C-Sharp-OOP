8f4ef049ba5b1f3a6ed65f94fc766e7d96d8bedc
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-02 22:14:15 +0300

    Add Team

---
+++ Encapsulation/LAB/04. Team/Team.cs
@@ -0,0 +1,41 @@
+﻿namespace PersonsInfo
+{
+    using System.Collections.Generic;
+
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> FirstTeam
+        {
+            get => this.firstTeam.AsReadOnly();
+        }
+
+        public IReadOnlyList<Person> ReserveTeam
+        {
+            get => this.reserveTeam.AsReadOnly();
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                this.firstTeam.Add(player);
+            }
+
+            else
+            {
+                this.reserveTeam.Add(player);
+            }
+        }
+    }
+}
