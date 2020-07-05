1740a67b6906fdc829f282841aecc59426095354
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-05 22:06:32 +0300

    Add Simple calculator(with Stacks)

---
+++ C# Advanced/Stacks and Queues/LAB/3. Simple Calculator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _3._Simple_Calculator
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            Stack<string> symbols = new Stack<string>(input.Reverse());
+            
+            int result = int.Parse(symbols.Pop());
+
+            while (symbols.Any())
+            {
+                string nextSymbol = symbols.Pop();
+
+                if (nextSymbol == "+")
+                {
+                    result += int.Parse(symbols.Pop());
+                }
+                else if (nextSymbol == "-")
+                {
+                    result -= int.Parse(symbols.Pop());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
