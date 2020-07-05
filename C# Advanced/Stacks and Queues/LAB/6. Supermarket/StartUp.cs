8b51938620db7b3e654f058f5a3085d491e70b0a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-05 22:07:24 +0300

    Add Supermarket

---
+++ C# Advanced/Stacks and Queues/LAB/6. Supermarket/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _6._Supermarket
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Queue<string> listOfCustomersNames = new Queue<string>();
+
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    List<string> listOfPaidCustomers = new List<string>();
+
+                    while (listOfCustomersNames.Count > 0)
+                    {
+                        listOfPaidCustomers.Add(listOfCustomersNames.Dequeue());
+                    }
+
+                    Console.WriteLine(string.Join(Environment.NewLine, listOfPaidCustomers));
+                }
+                else
+                {
+                    listOfCustomersNames.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{listOfCustomersNames.Count} people remaining.");
+        }
+    }
+}
