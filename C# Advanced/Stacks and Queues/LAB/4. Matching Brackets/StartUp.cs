0234f40b77bee29513008052129ce07dc062ed54
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-05 22:06:59 +0300

    Add Matching brackets

---
+++ C# Advanced/Stacks and Queues/LAB/4. Matching Brackets/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _4._Matching_Brackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
