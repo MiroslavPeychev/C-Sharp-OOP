79cb80d83d2f5faa59348b32589e2c5f31e0f5cf
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-05 22:07:52 +0300

    Add Hot potato

---
+++ C# Advanced/Stacks and Queues/LAB/7. Hot Potato/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _7._Hot_Potato
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] allChildren = Console.ReadLine()
+                .Split(' ');
+            int number = int.Parse(Console.ReadLine());
+
+            Queue<string> children = new Queue<string>(allChildren);
+            int counter = 1;
+
+            while (children.Count > 1)
+            {
+                var currentCild = children.Dequeue();
+
+                if (counter % number != 0)
+                {
+                    children.Enqueue(currentCild);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {currentCild}");
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
