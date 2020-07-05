2f57285ad13e4e4f9a521a28b29d626bdb4e18d7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-05 22:05:31 +0300

    Add Reverse strings (with Stacks)

---
+++ C# Advanced/Stacks and Queues/LAB/1. Reverse Strings/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._Reverse_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Stack<char> stack = new Stack<char>();
+            string input = Console.ReadLine();
+
+            foreach (char symbol in input)
+            {
+                stack.Push(symbol);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
