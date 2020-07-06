6a99188f8cf709012b96a9a43e0184ebc88c7380
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-06 21:30:12 +0300

    Add Basic Stack operations

---
+++ C# Advanced/Stacks and Queues/Exercise/01. Basic Stack Operations/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numbersToPush = input[0];
+            int numbersToPop = input[1];
+            int numbersToFind = input[2];
+
+            int[] elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < numbersToPush; i++)
+            {
+                stack.Push(elements[i]);
+            }
+
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(numbersToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+        }
+    }
+}
