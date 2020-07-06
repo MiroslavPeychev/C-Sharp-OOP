c8169bbbe020f32267afd04aabf7f05d6d380916
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-06 21:32:01 +0300

    Add Fast food

---
+++ C# Advanced/Stacks and Queues/Exercise/04. Fast Food/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace _04._Fast_Food
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(input);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                if (quantityOfFood <= 0)
+                {
+                    break;
+                }
+
+                if (quantityOfFood - orders.Peek() >= 0)
+                {
+                    quantityOfFood -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+    }
+}
