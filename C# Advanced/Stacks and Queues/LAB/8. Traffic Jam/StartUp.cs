05a2dfec159f85cc93d4bf97553bcdbcdf30a312
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-06 21:13:41 +0300

    Add Traffic jam

---
+++ C# Advanced/Stacks and Queues/LAB/8. Traffic Jam/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _8._Traffic_Jam
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var countPerGreenLight = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            var totalCarsPassed = 0;
+
+            while (true)
+            {
+                var currentLine = Console.ReadLine();
+
+                if (currentLine == "end")
+                {
+                    Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+                    break;
+                }
+
+                if (currentLine != "green")
+                {
+                    cars.Enqueue(currentLine);
+                }
+                else
+                {
+                    int carToPass = Math.Min(countPerGreenLight, cars.Count);
+
+                    for (int i = 0; i < carToPass; i++)
+                    {
+                        string currentCar = cars.Dequeue();
+                        Console.WriteLine($"{currentCar} passed!");
+
+                        totalCarsPassed++;
+                    }
+                }
+            }
+        }
+    }
+}
