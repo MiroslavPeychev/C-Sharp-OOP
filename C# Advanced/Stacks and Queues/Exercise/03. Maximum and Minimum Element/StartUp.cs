96ed433541fa1b4e320b19a46d0045e0e17bfd78
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-06 21:31:24 +0300

    Add Maximum and minimum element

---
+++ C# Advanced/Stacks and Queues/Exercise/03. Maximum and Minimum Element/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace _03._Maximum_and_Minimum_Element
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfinputLines = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < numberOfinputLines; i++)
+            {
+                string currentInput = Console.ReadLine();
+
+                switch (currentInput)
+                {
+                    case "2":
+                        if (numbers.Any())
+                        {
+                            numbers.Pop();
+                        }
+                        break;
+                    case "3":
+                        if (numbers.Any())
+                        {
+                            sb.AppendLine(numbers.Max().ToString());
+                        }
+                        break;
+                    case "4":
+                        if (numbers.Any())
+                        {
+                            sb.AppendLine(numbers.Min().ToString());
+                        }
+                        break;
+
+                    default:
+                        string[] lineData = currentInput.Split();
+                        int currentNum = int.Parse(lineData[1]);
+                        numbers.Push(currentNum);
+                        break;
+                }
+            }
+
+            sb.AppendLine(string.Join(", ", numbers));
+            Console.Write(sb);
+        }
+    }
+}
