34b33b7581accd253680bd0450a06fd7761055d3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-06 21:32:41 +0300

    Add Fashion boutique

---
+++ C# Advanced/Stacks and Queues/Exercise/05. Fashion Boutique/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _05._Fashion_Boutique
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes = new Stack<int>(inputNumbers);
+
+            int racks = 1;
+            int sum = 0;
+
+            while (clothes.Any())
+            {
+                if (sum + clothes.Peek() <= rackCapacity)
+                {
+                    sum += clothes.Pop();
+                }
+                else
+                {
+                    sum = 0;
+                    racks++;
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
