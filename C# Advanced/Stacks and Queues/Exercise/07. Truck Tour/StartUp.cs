f305853066594f7f2ed2d34e5f70eb5ef0e0d75a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-07 22:27:02 +0300

    Add Truck tour

---
+++ C# Advanced/Stacks and Queues/Exercise/07. Truck Tour/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace _07._Truck_Tour
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int pumpsCount = int.Parse(Console.ReadLine());
+            Queue<int> difference = new Queue<int>();
+
+            for (int i = 0; i < pumpsCount; i++)
+            {
+                int[] pumpProps = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                difference.Enqueue(pumpProps[0] - pumpProps[1]);
+            }
+
+            int index = 0;
+
+            while (true)
+            {
+                Queue<int> copyDifference = new Queue<int>(difference);
+                int fuel = -1;
+
+                while (copyDifference.Any())
+                {
+                    if (copyDifference.Peek() > 0 && fuel == -1)
+                    {
+                        fuel = copyDifference.Dequeue();
+                        difference.Enqueue(difference.Dequeue());
+                    }
+                    else if (copyDifference.Peek() < 0 && fuel == -1)
+                    {
+                        copyDifference.Enqueue(copyDifference.Dequeue());
+                        difference.Enqueue(difference.Dequeue());
+                        index++;
+                    }
+                    else
+                    {
+                        fuel += copyDifference.Dequeue();
+
+                        if (fuel < 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (fuel >= 0)
+                {
+                    Console.WriteLine(index);
+                    return;
+                }
+
+                index++;
+            }
+        }
+    }
+}
