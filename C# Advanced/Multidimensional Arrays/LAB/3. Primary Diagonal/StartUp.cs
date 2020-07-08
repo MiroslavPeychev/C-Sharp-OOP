6f2980db93e71d13dd43f57f3edca5be266c63c1
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-08 23:34:34 +0300

    Add Primary diagonal

---
+++ C# Advanced/Multidimensional Arrays/LAB/3. Primary Diagonal/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _3._Primary_Diagonal
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int sumDiagonal = 0;
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                sumDiagonal += matrix[i, i];
+            }
+
+            Console.WriteLine(sumDiagonal);
+        }
+    }
+}
