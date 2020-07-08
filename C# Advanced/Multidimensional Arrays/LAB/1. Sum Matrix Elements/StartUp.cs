28896d4730dfbec800596dba99cac9c486ed94fe
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-08 23:33:34 +0300

    Add Sum matrix elements

---
+++ C# Advanced/Multidimensional Arrays/LAB/1. Sum Matrix Elements/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace _1._Sum_Matrix_Elements
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = columnElements[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+
+            int sum = 0;
+
+            foreach (int element in matrix)
+            {
+                sum += element;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
