3d8d6b7cadc096823344d3f4ec2cb9cc0c293dc8
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-08 23:34:10 +0300

    Add Sum matrix columns

---
+++ C# Advanced/Multidimensional Arrays/LAB/2. Sum Matrix Columns/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _2._Sum_Matrix_Columns
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = columnElements[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
