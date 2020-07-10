24d92a271353dc678239318efa594ac98f8f42d3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-10 22:47:03 +0300

    Add Squares in matrix

---
+++ C# Advanced/Multidimensional Arrays/Exercise/2. Squares in Matrix/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace _2._Squares_in_Matrix
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            char[,] matrix = new char[sizes[0], sizes[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] row = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    char current = matrix[i, j];
+
+                    if (current == matrix[i, j + 1] && current == matrix[i + 1, j] && current == matrix[i + 1, j + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
