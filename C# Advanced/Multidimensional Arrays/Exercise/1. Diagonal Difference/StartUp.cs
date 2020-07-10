12f4bec0c4f11bb19922f14350f213411c065e84
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-10 22:46:29 +0300

    Add Diagonal difference

---
+++ C# Advanced/Multidimensional Arrays/Exercise/1. Diagonal Difference/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace _1._Diagonal_Difference
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+            long[,] matrix = new long[sizeMatrix, sizeMatrix];
+            long primaryDiagonal = 0;
+            long secondaryDiagonal = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                long[] numbersInCol = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = numbersInCol[j];
+
+                    if (j == i)
+                    {
+                        primaryDiagonal += matrix[i, j];
+                    }
+                    if (j == (sizeMatrix - 1 - i))
+                    {
+                        secondaryDiagonal += matrix[i, j];
+                    }
+                }
+            }
+
+            long difference = Math.Abs(primaryDiagonal - secondaryDiagonal);
+
+            Console.WriteLine(difference);
+        }
+    }
+}
