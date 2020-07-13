8b4a9a5471bb82f44ffcb9ca40cfd257d0731b12
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-13 23:02:56 +0300

    Add Car

---
+++ C# Advanced/Defining Classes/LAB/1. Car/Car.cs
@@ -0,0 +1,46 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class Car
+    {
+        
+        private int year;
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year 
+        {
+            get
+            { 
+                return this.year; 
+            }
+            set 
+            {
+                if (value < 1886)
+                {
+                    throw new InvalidOperationException("Year cannot be less than 1886.");
+                }
+
+                this.year = value;
+            }
+        }
+
+        public string Details
+        {
+            get 
+            { 
+                return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}";
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/LAB/1. Car/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Car car = new Car("VW", "MK3", 1992);
+            
+            Console.WriteLine(car.Details);
+        }
+    }
+}
