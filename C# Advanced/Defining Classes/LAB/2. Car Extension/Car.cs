fb3a4070a548e5c5522b70573279d88f9b6258de
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-13 23:03:24 +0300

    Add Car Extension

---
+++ C# Advanced/Defining Classes/LAB/2. Car Extension/Car.cs
@@ -0,0 +1,90 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Text;
+
+    public class Car
+    {
+        private int year;
+        private double fuelConsumption;
+        private double fuelQuantity;
+
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.fuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                if (value < 1886)
+                {
+                    throw new InvalidOperationException("Year cannot be less than 1886.");
+                }
+
+                this.year = value;
+            }
+        }
+
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.FuelConsumption;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("FuelQuantity must be a positive number.");
+                }
+
+                this.fuelQuantity = value;
+            }
+        }
+
+        public double FuelConsumption { get; private set; }
+
+        public void Drive(double distance)
+        {
+            var neededFuel = distance * this.FuelConsumption;
+
+
+            bool canContinue = this.FuelQuantity - neededFuel >= 0;
+
+            if (canContinue)
+            {
+                this.FuelQuantity -= neededFuel;
+            }
+            else
+            {
+                throw new InvalidOperationException("Not enough fuel.");
+            }
+        }
+
+        public string GetSpecifications()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/LAB/2. Car Extension/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Car car = new Car("VW", "MK3", 1992, 200, 200);
+
+            car.Drive(2000);
+
+            Console.WriteLine(car.GetSpecifications());
+        }
+    }
+}
