ad99c365823945c6ffe5c8b69a43bd6593159836
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:52:11 +0300

    Add Creating constructors

---
+++ C# Advanced/Defining Classes/Exercise/P02. Creating Constructors/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Person person = new Person(name, age);
+               
+                Console.WriteLine($"{person.Name} -> {person.Age}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
