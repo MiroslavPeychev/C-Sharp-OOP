1fabb5c3f5ca56c1849cc0de003a95629ecae9e1
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:52:50 +0300

    Add Oldest family member

---
+++ C# Advanced/Defining Classes/Exercise/P03. Oldest Family Member/Person.cs
@@ -0,0 +1,67 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            : this(age)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new InvalidOperationException("Name is invalid. Name should not be null or white space. Please enter a valid name");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Age must be a positive number");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age}";
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/P03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    string[] personArgs = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    string name = personArgs[0];
+                    int age = int.Parse(personArgs[1]);
+
+                    Person person = new Person(name, age);
+
+                    family.AddMember(person);
+                }
+
+                Console.WriteLine(family.GetOldestMember());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
