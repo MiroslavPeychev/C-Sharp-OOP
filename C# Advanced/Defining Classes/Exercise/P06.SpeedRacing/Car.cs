69bec670d5bd4553fad5f65ca088bc868a5e8c77
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:54:22 +0300

    Add Speed racing

---
+++ C# Advanced/Defining Classes/Exercise/P06.SpeedRacing/Car.cs
@@ -0,0 +1,103 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = travelledDistance;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new InvalidOperationException("Model cannot be null or white space. Please, enter valid model.");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public double FuelAmount
+        {
+            get
+            {
+                return this.fuelAmount;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Fuel amount must be a positive number.");
+                }
+
+                this.fuelAmount = value;
+            }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidOperationException("Fuel consumption per kilometer must be a positive number");
+                }
+
+                this.fuelConsumptionPerKilometer = value;
+            }
+        }
+
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Traveled kilometers cannot be negative number");
+                }
+
+                this.travelledDistance = value;
+            }
+        }
+
+        public void DriveCar(int amountOfKm)
+        {
+            double neededFuel = this.FuelConsumptionPerKilometer * amountOfKm;
+
+            bool canMove = this.FuelAmount - neededFuel >= 0;
+
+            if (canMove)
+            {
+                this.FuelAmount -= neededFuel;
+                this.travelledDistance += amountOfKm;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:F2} {this.TravelledDistance}";
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/P06.SpeedRacing/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            HashSet<Car> cars = new HashSet<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumptionPerKilometer = double.Parse(carInfo[2]);
+
+                Car currentCar = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+
+                cars.Add(currentCar);
+            }
+
+            while (true)
+            {
+                try
+                {
+                    string[] commandLine = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    string command = commandLine[0];
+
+                    if (command == "End")
+                    {
+                        break;
+                    }
+
+                    if (command == "Drive")
+                    {
+                        string carModel = commandLine[1];
+                        int amountOfKm = int.Parse(commandLine[2]);
+
+                        Car currentCar = cars
+                            .Where(c => c.Model == carModel)
+                            .FirstOrDefault();
+
+                        currentCar.DriveCar(amountOfKm);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
