f639af96d0dd1ed49f3ca7aee65b5c43638f4ccf
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:53:16 +0300

    Add Opinion poll

---
+++ C# Advanced/Defining Classes/Exercise/P04.Opinion Poll/Family.cs
@@ -0,0 +1,37 @@
+﻿namespace DefiningClasses
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Family
+    {
+        private readonly HashSet<Person> members;
+
+        public Family()
+        {
+            this.members = new HashSet<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = this.members
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return oldestPerson;
+        }
+
+        public HashSet<Person> GetAllPeopleAbove30()
+        {
+            return this.members
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToHashSet();
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/P04.Opinion Poll/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            int membersCount = int.Parse(Console.ReadLine());
+
+            try
+            {
+                for (int i = 0; i < membersCount; i++)
+                {
+                    string[] personArgs = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    string name = personArgs[0];
+                    int age = int.Parse(personArgs[1]);
+
+                    Person person = new Person(name, age);
+
+                    family.AddMember(person);
+                }
+
+                HashSet<Person> result = family.GetAllPeopleAbove30();
+                Console.WriteLine(string.Join(Environment.NewLine, result));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
