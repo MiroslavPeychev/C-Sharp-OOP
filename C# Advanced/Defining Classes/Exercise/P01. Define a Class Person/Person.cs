a6467d793f1ef1f6e26cc80ce02e6f918cbdb263
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:51:41 +0300

    Add Define a class person

---
+++ C# Advanced/Defining Classes/Exercise/P01. Define a Class Person/Person.cs
@@ -0,0 +1,54 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new InvalidOperationException("Name is invalid. Name should not be null or white space. Please enter a valid name");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Age must be a positive number");
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/P01. Define a Class Person/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = "Pesho";
+            int age = 20;
+
+            try
+            {
+                Person person = new Person
+                {
+                    Name = name,
+                    Age = age
+                };
+
+                Console.WriteLine($"{person.Name} -> {person.Age}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
