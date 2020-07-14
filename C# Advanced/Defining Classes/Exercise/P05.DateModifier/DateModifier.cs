546dd19bc7a1df7496e8b76f293a59f422eb2e46
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-14 20:53:50 +0300

    Add Date modifier

---
+++ C# Advanced/Defining Classes/Exercise/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Globalization;
+
+    public class DateModifier
+    {
+        public static double GetDaysBetweenTwoDates(string dateOne, string dateTwo)
+        {
+            DateTime firstDate = DateTime.ParseExact(dateOne, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact(dateTwo, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            if (firstDate>secondDate)
+            {
+                return GetDaysBetweenTwoDates(dateTwo, dateOne);
+            }
+
+            return (secondDate - firstDate).Days;
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/P05.DateModifier/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            Console.WriteLine(Math.Abs(DateModifier.GetDaysBetweenTwoDates(firstDate, secondDate)));
+        }
+    }
+}
