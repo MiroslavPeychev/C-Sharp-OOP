a00f8c46ee1b6c060e77a81e429e22bfd9244243
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-16 12:49:46 +0300

    Update StartUp - add some variables for more readable code

--- C# Advanced/Defining Classes/Exercise/P08.CarSalesman/StartUp.cs
+++ C# Advanced/Defining Classes/Exercise/P08.CarSalesman/StartUp.cs
@@ -13,96 +13,106 @@
 
             int numberOfinputEngines = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < numberOfinputEngines; i++)
+            try
             {
-                string[] engineArgs = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .ToArray();
-
-                Engine engine = null;
-
-                string model = engineArgs[0];
-                int power = int.Parse(engineArgs[1]);
-
-                if (engineArgs.Length == 4)
+                for (int i = 0; i < numberOfinputEngines; i++)
                 {
-                    int displacement = int.Parse(engineArgs[2]);
-                    string efficiency = engineArgs[3];
+                    string[] engineArgs = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
 
-                    engine = new Engine(model, power, displacement, efficiency);
-                }
-                else if (engineArgs.Length == 3)
-                {
-                    int displacement;
+                    Engine engine = null;
 
-                    bool isDisplacement = int.TryParse(engineArgs[2], out displacement);
+                    string model = engineArgs[0];
+                    int power = int.Parse(engineArgs[1]);
 
-                    if (isDisplacement)
+                    if (engineArgs.Length == 4)
                     {
-                        engine = new Engine(model, power, displacement);
+                        int displacement = int.Parse(engineArgs[2]);
+                        string efficiency = engineArgs[3];
+
+                        engine = new Engine(model, power, displacement, efficiency);
                     }
-                    else
+                    else if (engineArgs.Length == 3)
                     {
-                        engine = new Engine(model, power, engineArgs[2]);
+                        int displacement;
+
+                        bool isDisplacement = int.TryParse(engineArgs[2], out displacement);
+
+                        if (isDisplacement)
+                        {
+                            engine = new Engine(model, power, displacement);
+                        }
+                        else
+                        {
+                            string efficiency = engineArgs[2];
+                            engine = new Engine(model, power, efficiency);
+                        }
                     }
-                }
-                else if (engineArgs.Length == 2)
-                {
-                    engine = new Engine(model, power);
+                    else if (engineArgs.Length == 2)
+                    {
+                        engine = new Engine(model, power);
+                    }
+
+                    if (engine != null)
+                    {
+                        engines.Add(engine);
+                    }
                 }
 
-                if (engine != null)
+                int numberOfCars = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < numberOfCars; i++)
                 {
-                    engines.Add(engine);
+                    string[] carArg = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    Car car = null;
+
+                    string model = carArg[0];
+                    string inputEngineModel = carArg[1];
+                    Engine engine = engines.First(e => e.Model == inputEngineModel);
+
+                    if (carArg.Length == 2)
+                    {
+                        car = new Car(model, engine);
+                    }
+                    else if (carArg.Length == 3)
+                    {
+                        double weight;
+
+                        bool isWeight = double.TryParse(carArg[2], out weight);
+
+                        if (isWeight)
+                        {
+                            car = new Car(model, engine, weight);
+                        }
+                        else
+                        {
+                            string color = carArg[2];
+                            car = new Car(model, engine, color);
+                        }
+                    }
+                    else if (carArg.Length == 4)
+                    {
+                        double weight = double.Parse(carArg[2]);
+                        string color = carArg[3];
+
+                        car = new Car(model, engine, weight, color);
+                    }
+
+                    if (car != null)
+                    {
+                        cars.Add(car);
+                    }
                 }
             }
-
-            int numberOfCars = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < numberOfCars; i++)
+            catch (Exception ex)
             {
-                string[] carArg = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .ToArray();
 
-                Car car = null;
-
-                string model = carArg[0];
-                Engine engine = engines.First(e => e.Model == carArg[1]);
-
-                if (carArg.Length == 2)
-                {
-                    car = new Car(model, engine);
-                }
-                else if (carArg.Length == 3)
-                {
-                    double weight;
-
-                    bool isWeight = double.TryParse(carArg[2], out weight);
-
-                    if (isWeight)
-                    {
-                        car = new Car(model, engine, weight);
-                    }
-                    else
-                    {
-                        car = new Car(model, engine, carArg[2]);
-                    }
-                }
-                else if (carArg.Length == 4)
-                {
-                    double weight = double.Parse(carArg[2]);
-                    string color = carArg[3];
-
-                    car = new Car(model, engine, weight, color);
-                }
-
-                if (car != null)
-                {
-                    cars.Add(car);
-                }
+                Console.WriteLine(ex.Message);
             }
-
 
             foreach (Car car in cars)
             {
