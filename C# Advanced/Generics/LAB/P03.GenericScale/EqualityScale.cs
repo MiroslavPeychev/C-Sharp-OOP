7c250c2fef95b657f9ab2022036426bf41ab9250
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-17 22:50:38 +0300

    Add Generics/LAB/Generic Scale

---
+++ C# Advanced/Generics/LAB/P03.GenericScale/EqualityScale.cs
@@ -0,0 +1,34 @@
+﻿namespace GenericScale
+{
+    using System;
+
+    public class EqualityScale<T>
+        where T : IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public T GetHeavier()
+        {
+            int comparison = left.CompareTo(right);
+
+            if (comparison == 1)
+            {
+                return left;
+            }
+            else if (comparison == -1)
+            {
+                return right;
+            }
+
+            return default(T);
+
+        }
+    }
+}
---
+++ C# Advanced/Generics/LAB/P03.GenericScale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericScale
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var scale1 = new EqualityScale<int>(5, 10);
+            Console.WriteLine(scale1.GetHeavier());
+
+            var scale2 = new EqualityScale<string>("Man", "Woman");
+            Console.WriteLine(scale2.GetHeavier());
+        }
+    }
+}
