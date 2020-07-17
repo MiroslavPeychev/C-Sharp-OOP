3041623f5af4f02925a6b3d44dbdf87774b06e96
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-17 22:57:24 +0300

    Add Generics/Exercise/GenericBoxOfInteger

---
+++ C# Advanced/Generics/Exercise/P02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace P02.GenericBoxOfInteger
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                Box<int> boxWithInt = new Box<int>(value);
+
+                Console.WriteLine(boxWithInt);
+            }
+        }
+    }
+}
