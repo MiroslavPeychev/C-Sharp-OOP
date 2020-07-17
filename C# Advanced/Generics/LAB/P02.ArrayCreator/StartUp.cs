b996dcd275b6c823a19811eefdc384caf17b8754
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-17 22:49:58 +0300

    Add Generics/LAB/ArrayCreator

---
+++ C# Advanced/Generics/LAB/P02.ArrayCreator/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericArrayCreator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(string.Join(" ", strings));
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
