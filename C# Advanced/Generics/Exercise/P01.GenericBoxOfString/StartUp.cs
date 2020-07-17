06cdcba61ddb3a8d9dcf9dce842a91d3c7f5aa50
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-17 22:56:33 +0300

    Add Generics/Exercise/GenericBoxOfString

---
+++ C# Advanced/Generics/Exercise/P01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace P01.GenericBoxOfString
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string value = Console.ReadLine();
+
+                Box<string> boxWithString = new Box<string>(value);
+
+                Console.WriteLine(boxWithString);
+            }
+        }
+    }
+}
