af569a0b8b0253379bb1e4521bbd52ddcf4d1af6
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-17 22:49:12 +0300

    Add Generics/LAB/01. Box

---
+++ C# Advanced/Generics/LAB/01. Box/Box.cs
@@ -0,0 +1,32 @@
+﻿namespace BoxOfT
+{
+    using System.Collections.Generic;
+
+    public class Box<T>
+    {
+        private Stack<T> data;
+
+        public Box()
+        {
+            this.data = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+        
+        public void Add(T item)
+        {
+            this.data.Push(item);
+        }
+
+        public T Remove()
+        {
+           return this.data.Pop();
+        }
+    }
+}
