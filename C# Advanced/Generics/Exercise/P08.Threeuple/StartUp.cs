64e2eb7d996d06078c75a868737c3f716047e147
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-18 18:44:12 +0300

    Add Generics/Exercise/Threeuple

---
+++ C# Advanced/Generics/Exercise/P08.Threeuple/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace P08.Threeuple
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] firstInput = SplitInput();
+
+            string firstName = firstInput[0];
+            string lastName = firstInput[1];
+            string address = firstInput[2];
+            string fullName = $"{firstName} {lastName}";
+            string town = string.Empty;
+
+            if (firstInput.Length >= 5)
+            {
+                town = firstInput[3] + " " + firstInput[4];
+            }
+            else
+            {
+                town = firstInput[3];
+            }
+
+            string[] secondInput = SplitInput();
+
+            string name = secondInput[0];
+            int litersBeerToDrink = int.Parse(secondInput[1]);
+            bool isDrunk = secondInput[2] == "drunk" ? true : false;
+
+            string[] thirdInput = SplitInput();
+
+            string thirdInputName = thirdInput[0];
+            double accountBalance = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+
+            Tuple<string, string, string> firstTuple = new Tuple<string, string, string>(fullName, address, town);
+            Tuple<string, int, bool> secondTuple = new Tuple<string, int, bool>(name, litersBeerToDrink, isDrunk);
+            Tuple<string, double, string> thirdTuple = new Tuple<string, double, string>(thirdInputName, accountBalance, bankName);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+
+        public static string[] SplitInput()
+        {
+            string[] splittedInput = Console.ReadLine()
+                .Split();
+
+            return splittedInput;
+        }
+    }
+}
---
+++ C# Advanced/Generics/Exercise/P08.Threeuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿namespace P08.Threeuple
+{
+    public class Tuple<TFirst, TSecond, TThird>
+    {
+        public Tuple(TFirst firstItem, TSecond secondItem, TThird thirdItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecondItem = secondItem;
+            this.ThirdItem = thirdItem;
+        }
+
+        public TFirst FirstItem { get; set; }
+
+        public TSecond SecondItem { get; set; }
+
+        public TThird ThirdItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem} -> {this.ThirdItem}"; 
+        }
+    }
+}
