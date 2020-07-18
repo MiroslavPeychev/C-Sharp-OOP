a6f7e90a331bb5523954799b7186eac3ff4bda28
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-18 18:41:56 +0300

    Add Generics/Exercise/GenericCountMethodString

---
+++ C# Advanced/Generics/Exercise/P05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace P05.GenericCountMethodString
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfElements = int.Parse(Console.ReadLine());
+            List<string> elements = new List<string>();
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                string currentElement = Console.ReadLine();
+
+                elements.Add(currentElement);
+            }
+
+            Box<string> box = new Box<string>(elements);
+
+            string elementToCompare = Console.ReadLine();
+
+            int countOfGreaterElements = box.CountGreaterElements(elements, elementToCompare);
+
+            Console.WriteLine(countOfGreaterElements);
+
+        }
+    }
+}
