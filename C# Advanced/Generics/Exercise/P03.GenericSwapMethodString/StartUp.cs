fb0a8de2237defca0f75af599f00f7e0824123f4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-18 18:40:56 +0300

    Add Generics/Exercise/GenericSwapMethodString

---
+++ C# Advanced/Generics/Exercise/P03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,37 @@
+﻿
+namespace P03.GenericSwapMethodString
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            Box<string> box = new Box<string>(names);
+
+            int[] indexesToSwap = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexesToSwap[0];
+            int secondIndex = indexesToSwap[1];
+
+            box.Swap(names, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+    }
+}
