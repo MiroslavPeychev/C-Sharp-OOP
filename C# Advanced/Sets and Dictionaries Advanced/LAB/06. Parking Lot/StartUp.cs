e936ae714d3b6de7d2283ef19fa6718ff8f370ef
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-20 23:14:24 +0300

    Add Parking lot

---
+++ C# Advanced/Sets and Dictionaries Advanced/LAB/06. Parking Lot/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace _06._Parking_Lot
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            HashSet<string> listOfCars = new HashSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = input
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = tokens[0];
+                string carPlate = tokens[1];
+
+                if (command == "IN")
+                {
+                    listOfCars.Add(carPlate);
+                }
+                else if (command == "OUT")
+                {
+                    if (listOfCars.Contains(carPlate))
+                    {
+                        listOfCars.Remove(carPlate);
+                    }
+                }
+            }
+
+            if (listOfCars.Count > 0)
+            {
+                foreach (var item in listOfCars)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
