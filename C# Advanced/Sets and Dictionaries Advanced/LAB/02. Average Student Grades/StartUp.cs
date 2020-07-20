937b0a871ab3eee66134d0cc427e1e8578bc9a44
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-20 23:09:53 +0300

    Add Average student grade

---
+++ C# Advanced/Sets and Dictionaries Advanced/LAB/02. Average Student Grades/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace _02._Average_Student_Grades
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var marks = new Dictionary<string, List<double>>();
+
+            int totalMarks = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < totalMarks; i++)
+            {
+                var markParts = Console.ReadLine().Split();
+                var name = markParts[0];
+                var mark = double.Parse(markParts[1]);
+
+                if (!marks.ContainsKey(name))
+                {
+                    marks[name] = new List<double>();
+                }
+
+                marks[name].Add(mark);
+            }
+
+            foreach (var kvp in marks)
+            {
+                var name = kvp.Key;
+                var currentMarks = kvp.Value;
+                var averageMark = currentMarks.Average();
+
+                Console.Write($"{name} -> ");
+                foreach (var mark in currentMarks)
+                {
+                    Console.Write($"{mark:f2} ");
+
+                }
+
+                Console.WriteLine($"(avg: {averageMark:f2})");
+
+                //Console.WriteLine($"{name} -> {string.Join(" ", currentMarks):f2} (avg: {averageMark:F2})");
+            }
+        }
+    }
+}
