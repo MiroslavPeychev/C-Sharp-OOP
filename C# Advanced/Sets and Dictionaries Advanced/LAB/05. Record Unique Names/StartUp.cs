2976bf4fd7e1c859861239ee35a5aeaa273db8b6
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-20 23:13:51 +0300

    Add Record unique names

---
+++ C# Advanced/Sets and Dictionaries Advanced/LAB/05. Record Unique Names/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Record_Unique_Names
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> listOfNames = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                listOfNames.Add(name);
+            }
+
+            foreach (var name in listOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
