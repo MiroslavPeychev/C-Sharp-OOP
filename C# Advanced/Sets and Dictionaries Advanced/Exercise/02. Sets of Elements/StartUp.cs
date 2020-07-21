fd83bc3f34e51fae34c54f1f4f417f33767bbe82
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-21 23:10:02 +0300

    Add Sets of elements

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/02. Sets of Elements/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _02._Sets_of_Elements
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse).ToArray();
+
+            var n = input[0];
+            var m = input[1];
+
+            var setN = new HashSet<int>();
+            var setM = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                setN.Add(number);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                setM.Add(number);
+            }
+
+            setN.IntersectWith(setM);
+
+            Console.WriteLine(string.Join(" ", setN));
+        }
+    }
+}
