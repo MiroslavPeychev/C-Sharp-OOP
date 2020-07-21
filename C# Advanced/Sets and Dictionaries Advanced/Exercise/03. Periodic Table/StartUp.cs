c99b5aa9d8a3796761c0e6c1b07a23ea6ca08880
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-21 23:10:39 +0300

    Add Periodic table

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/03. Periodic Table/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _03._Periodic_Table
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            HashSet<string> list = new HashSet<string>();
+
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] tokens = Console
+                    .ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int j = 0; j < tokens.Length; j++)
+                {
+                    list.Add(tokens[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", list.OrderBy(x => x)));
+        }
+    }
+}
