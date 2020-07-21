54b0d4498fad075fa3f77580dd305913c462f2e7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-21 23:11:49 +0300

    Add Count symbols

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/05. Count Symbols/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _05._Count_Symbols
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string inputLine = Console.ReadLine();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            foreach (var @char in inputLine)
+            {
+                if (symbols.ContainsKey(@char) == false)
+                {
+                    symbols[@char] = 0;
+                }
+
+                symbols[@char]++;
+            }
+
+            foreach (var kvp in symbols)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
