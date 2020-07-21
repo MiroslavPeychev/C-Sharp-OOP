0b35792b7a640e5e5c5702c2e4f7aac408bea3ee
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-21 23:09:33 +0300

    Add Unique usernames

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/01. Unique Usernames/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Unique_Usernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            HashSet<string> listOfNames = new HashSet<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                listOfNames.Add(Console.ReadLine());
+            }
+
+            foreach (string name in listOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
