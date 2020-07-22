040670c9f52ca68fbaccd417fd8b0416af222175
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-22 23:34:47 +0300

    Add Sum numbers

---
+++ C# Advanced/Functional Programming/LAB/02. Sum Numbers/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _02._Sum_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Count());
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
