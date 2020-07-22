cb7cd0493da010414ca82be83c105f5f1bb7b902
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-22 23:34:19 +0300

    Add Sort even numbers

---
+++ C# Advanced/Functional Programming/LAB/01. Sort Even Numbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            string result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
