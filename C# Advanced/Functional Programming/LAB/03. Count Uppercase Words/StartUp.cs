de7ca208f6a39b213e738a484f7acc3230f65e62
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-22 23:35:20 +0300

    Add Count uppercase words

---
+++ C# Advanced/Functional Programming/LAB/03. Count Uppercase Words/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+
+            => Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => char.IsUpper(x[0]))
+            .ToList()
+            .ForEach(Console.WriteLine);
+    }
+}
