dc528d939482e917fd14786c4c1503d043099a28
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-22 23:41:04 +0300

    Add Add VAT

---
+++ C# Advanced/Functional Programming/LAB/04. Add VAT/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Add_VAT
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double current = input[i];
+                double withVAT = current * 1.20;
+
+                Console.WriteLine($"{withVAT:f2}");
+            }
+        }
+    }   
+}
