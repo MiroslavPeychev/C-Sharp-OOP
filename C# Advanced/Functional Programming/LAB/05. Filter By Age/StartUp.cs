5e62dd512681a21d1e68708f9ed31c1ce5842292
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-22 23:35:54 +0300

    Add Filter by age

---
+++ C# Advanced/Functional Programming/LAB/05. Filter By Age/StartUp.cs
@@ -0,0 +1,72 @@
+﻿namespace _05._Filter_By_Age
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int totalPeople = int.Parse(Console.ReadLine());
+
+            var people = new List<Person>();
+
+            for (int i = 0; i < totalPeople; i++)
+            {
+                var currentPerson = Console.ReadLine()
+                    .Split(
+                    new[] { ',', ' ' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                var person = new Person
+                {
+                    Name = currentPerson[0],
+                    Age = int.Parse(currentPerson[1])
+                };
+
+                people.Add(person);
+            }
+
+            string condition = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Func<Person, bool> filterPredicate;
+
+            if (condition == "older")
+            {
+                filterPredicate = p => p.Age >= age;
+            }
+            else
+            {
+                filterPredicate = p => p.Age < age;
+            }
+
+            string format = Console.ReadLine();
+
+            Func<Person, string> selectFunc;
+
+            if (format == "name age")
+            {
+                selectFunc = p => $"{p.Name} - {p.Age}";
+            }
+            else
+            {
+                selectFunc = p => $"{p.Name}";
+            }
+
+            people
+                .Where(filterPredicate)
+                .Select(selectFunc)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
