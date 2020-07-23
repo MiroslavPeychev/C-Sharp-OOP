43baa5ede0b1fcffe90045de2e19fd6cedd2e284
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-23 22:52:30 +0300

    Add Action print

---
+++ C# Advanced/Functional Programming/Exercise/01. Action Print/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _01._Action_Print
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Action<string> print = name => Console.WriteLine(name);
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);
+        }
+    }
+}
