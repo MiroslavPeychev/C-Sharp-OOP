a6be9a7c3fd8b5ce4153e51ed1a23a1246c3c65e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-23 22:56:29 +0300

    Add Reverse and exclude

---
+++ C# Advanced/Functional Programming/Exercise/06. Reverse And Exclude/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => x % number != 0)));
+        }
+    }
+}
