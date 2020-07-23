3d7ec92cb526b88e9f718be3eef23df14857b1fa
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-23 22:53:35 +0300

    Add Custom min function

---
+++ C# Advanced/Functional Programming/Exercise/03. Custom Min Function/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Custom_Min_Function
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Func<int[], int> minNumber = n => n.Min();
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minNumber(numbers));
+        }
+    }
+}
