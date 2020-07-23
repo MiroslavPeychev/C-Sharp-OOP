d1267e3d56a247c5a51c03ae18aad5dec8b59d0e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-23 22:54:19 +0300

    Add Find evens or odds

---
+++ C# Advanced/Functional Programming/Exercise/04. Find Evens or Odds/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _04._Find_Evens_or_Odds
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] rangeArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = rangeArgs[0];
+            int end = rangeArgs[1];
+
+            string typeNumbers = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+
+            Predicate<int> filter = x => typeNumbers == "odd" ? x % 2 != 0 : x % 2 == 0;
+            Func<int, bool> filterFunc = x => typeNumbers == "odd" ? x % 2 != 0 : x % 2 == 0;
+
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers.Where(filterFunc)));
+        }
+    }
+}
