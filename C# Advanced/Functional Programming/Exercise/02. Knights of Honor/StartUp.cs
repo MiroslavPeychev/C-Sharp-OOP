bc204c24bb0eccac84f082a2410cacce94aa6d93
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-23 22:53:00 +0300

    Add Knights of honor

---
+++ C# Advanced/Functional Programming/Exercise/02. Knights of Honor/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Knights_of_Honor
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => $"Sir {n}")
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
