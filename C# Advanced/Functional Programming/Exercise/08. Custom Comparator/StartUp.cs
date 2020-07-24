dea59cb73aa5c9f1591c08008a610b4036f82eed
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-24 23:00:17 +0300

    Add Custom comparator

---
+++ C# Advanced/Functional Programming/Exercise/08. Custom Comparator/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _08._Custom_Comparator
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> sortFunc = (a, b) => a.CompareTo(b);
+            Action<int[], int[]> print = (x, y) => Console.WriteLine($"{string.Join(" ", x)} {string.Join(" ", y)}");
+
+            int[] evenNumbers = numbers.Where(x => x % 2 == 0).ToArray();
+            int[] oddNumbers = numbers.Where(x => x % 2 != 0).ToArray();
+
+            Array.Sort(evenNumbers, new Comparison<int>(sortFunc));
+            Array.Sort(oddNumbers, new Comparison<int>(sortFunc));
+
+            print(evenNumbers, oddNumbers);
+        }
+    }
+}
