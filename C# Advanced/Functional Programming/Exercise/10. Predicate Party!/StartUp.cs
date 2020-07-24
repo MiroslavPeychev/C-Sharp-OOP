5290313580abc3e3ff80f09ba46f69a123e9c620
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-24 23:01:13 +0300

    Add Predicate Party!

---
+++ C# Advanced/Functional Programming/Exercise/10. Predicate Party!/StartUp.cs
@@ -0,0 +1,72 @@
+﻿namespace _10._Predicate_Party_
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> guests = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Party!")
+            {
+                string[] tokens = input.Split();
+
+                string command = tokens[0];
+                string filterCommand = tokens[1];
+                string criteria = tokens[2];
+
+                if (command == "Remove")
+                {
+                    if (filterCommand == "StartsWith")
+                    {
+                        guests.RemoveAll(x => x.StartsWith(criteria));
+                    }
+                    else if (filterCommand == "EndsWith")
+                    {
+                        guests.RemoveAll(x => x.EndsWith(criteria));
+                    }
+                    else if (filterCommand == "Length")
+                    {
+                        guests.RemoveAll(x => x.Length == int.Parse(criteria));
+                    }
+
+                }
+                else if (command == "Double")
+                {
+                    List<string> guestsToAdd = new List<string>();
+
+                    if (filterCommand == "StartsWith")
+                    {
+                        guestsToAdd = guests.Where(x => x.StartsWith(criteria)).ToList();
+                    }
+                    else if (filterCommand == "EndsWith")
+                    {
+                        guestsToAdd = guests.Where(x => x.EndsWith(criteria)).ToList();
+                    }
+                    else if (filterCommand == "Length")
+                    {
+                        guestsToAdd = guests.Where(x => x.Length == int.Parse(criteria)).ToList();
+                    }
+
+                    foreach (var name in guestsToAdd)
+                    {
+                        int index = guests.IndexOf(name);
+
+                        guests.Insert(index + 1, name);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Any() ? $"{string.Join(", ", guests)} are going to the party!" : "Nobody is going to the party!");
+        }
+    }
+}
