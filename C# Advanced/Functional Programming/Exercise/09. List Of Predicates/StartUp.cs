4dd9e194e7acdc8411aa3719d099d23a7df45ded
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-24 23:00:43 +0300

    Add List of predicates

---
+++ C# Advanced/Functional Programming/Exercise/09. List Of Predicates/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace _09._List_Of_Predicates
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Func<int, int, bool> isNotDivisible = (x, y) => x % y != 0;
+
+            Action<List<int>> printResultNumbers = n => Console.WriteLine(string.Join(" ", n));
+
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isValidNumber = true;
+
+            List<int> result = new List<int>();
+
+            for (int currentNumber = 1; currentNumber <= number; currentNumber++)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (isNotDivisible(currentNumber, numbers[i]))
+                    {
+                        isValidNumber = false;
+                        break;
+                    }
+                }
+
+                if (isValidNumber)
+                {
+                    result.Add(currentNumber);
+                }
+
+                isValidNumber = true;
+            }
+
+            printResultNumbers(result);
+        }
+    }
+}
