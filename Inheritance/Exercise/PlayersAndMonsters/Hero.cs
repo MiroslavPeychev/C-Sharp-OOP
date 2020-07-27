cb824b7fb1df7a2676fc674fd4c7e97214cc6635
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-27 23:48:10 +0300

    Add Inheritance/Exercise/PlayersAndMonsters

---
+++ Inheritance/Exercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public abstract class Hero
+    {
+        private const int HERO_MIN_LEVEL = 0;
+
+        private string username;
+        private int level;
+
+        protected Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Username cannot be null or whitespace");
+                }
+
+                this.username = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return this.Level;
+            }
+            private set
+            {
+                if (value < HERO_MIN_LEVEL)
+                {
+                    throw new ArgumentException("Level cannot be a negative number");
+                }
+
+                this.level = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+
+    }
+}
