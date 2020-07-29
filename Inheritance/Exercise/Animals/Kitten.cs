3b93d3b2000badcf978ea1184a8e44742335b06f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-29 23:53:04 +0300

    Add Animals

---
+++ Inheritance/Exercise/Animals/Kitten.cs
@@ -0,0 +1,16 @@
+﻿namespace Animals
+{
+    public class Kitten : Cat
+    {
+        private const string DEFAULT_GENDER = "Female";
+        public Kitten(string name, int age, string gender)
+            : base(name, age, DEFAULT_GENDER)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
---
+++ Inheritance/Exercise/Animals/TomCat.cs
@@ -0,0 +1,16 @@
+﻿namespace Animals
+{
+    public class TomCat : Cat
+    {
+        private const string DEFAULT_GENDER = "Male";
+        public TomCat(string name, int age)
+            : base(name, age, DEFAULT_GENDER)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
