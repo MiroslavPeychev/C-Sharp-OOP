38690a7ad17276088ba10646c12ee26276e35ab7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-07-31 23:59:18 +0300

    Add NeedForSpeed

---
+++ Inheritance/Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace NeedForSpeed
+{
+    public abstract class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        protected Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; private set; }
+
+        public double Fuel { get; private set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelNeeded = this.FuelConsumption * kilometers;
+            if (this.Fuel>=fuelNeeded)
+            {
+                this.Fuel -= fuelNeeded;
+            }
+        }
+    }
+}
