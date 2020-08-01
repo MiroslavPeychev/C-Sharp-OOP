4e53634c456ded527e6538c4e95eade5d36329c1
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-08-01 23:25:31 +0300

    Modified class Tomcat

--- Inheritance/Exercise/Animals/TomCat.cs
+++ Inheritance/Exercise/Animals/TomCat.cs
@@ -3,6 +3,7 @@
     public class Tomcat : Cat
     {
         private const string DEFAULT_GENDER = "Male";
+
         public Tomcat(string name, int age)
             : base(name, age, DEFAULT_GENDER)
         {
